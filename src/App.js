a9e227d11cbc613826c7f2cca0971759ee6cd7ca
VanessaTrajano <vanessapalmeirakelmertrajano@gmail.com>
2022-02-12 18:36:51 -0300

    Subindo o desafio do contador

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Contador extends Component{
+  state ={
+    number: 5,
+    msg: ":)"
+  }
+
+  add = () => {
+    if(this.state.number < 10){
+      this.setState({
+        number: this.state.number + 1,
+        msg: ":)"
+      })
+      return this.state.number
+    } else {
+      this.setState({
+        msg: "A soma não pode ser maior que 10",
+      })
+    }
+  }
+
+  remove = () => {
+    if(this.state.number > 0){
+      this.setState({
+        number: this.state.number - 1,
+        msg: ":)"
+      })
+      return this.state.number
+    } else {
+      this.setState({
+        msg: "A subtração não pode ser menor que 0",
+      })
+    }
+  };
+
+  render(){
+    return(
+      <div className='box'>
+        <button onClick={this.add}> Somar </button>
+        <h1>{this.state.number}</h1>
+        <button onClick={this.remove}> Subtrair </button>
+        <h2>{this.state.msg}</h2>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Contador
